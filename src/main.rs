219e9dd41ae9d721da5a124b1b6017688a8e7620
fteychene <francois.teychene@gmail.com>
2020-04-05 23:45:44 +0200

    feat: Add error library and examples for error management

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+use anyhow::{Context, Error};
 use serde::Deserialize;
 
-fn main() {
+fn main() -> Result<(), Error> {
     println!("Hello from dhall mock project 👋");
 
     // Some Dhall data
     let data = r###"
         let Mock = ./dhall/Mock/package.dhall
-        in Mock.HttpMethod.GET
+        in Mock.HttpMethod.UNKNOW
     "###;
 
     // Deserialize it to a Rust type.
-    let method: HttpMethod = serde_dhall::from_str(data).parse().unwrap();
+    let method: HttpMethod = serde_dhall::from_str(data)
+        .parse()
+        .context("Parsing dhall configuration")?;
 
     assert_eq!(method, HttpMethod::GET);
+
+    Ok(())
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
