5d6a9660fd7a6f20e42a7066b9602412ec9655d5
rlecomte <romain.lecomte.pro@gmail.com>
2020-04-05 22:42:38 +0200

    first draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use serde::Deserialize;
+
 fn main() {
     println!("Hello from dhall mock project 👋");
+
+    // Some Dhall data
+    let data = r###"
+        let Mock = ./dhall/Mock/package.dhall
+        in Mock.HttpMethod.GET
+    "###;
+
+    // Deserialize it to a Rust type.
+    let method: HttpMethod = serde_dhall::from_str(data).parse().unwrap();
+
+    assert_eq!(method, HttpMethod::GET);
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+enum HttpMethod {
+    HEAD,
+    GET,
+    PUT,
+    POST,
 }
