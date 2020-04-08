d8b046e40f8cbbedc3be2fbd1f4535ef16d5c2ec
fteychene <francois.teychene@gmail.com>
2020-04-09 00:24:16 +0200

    feat: Add cli args using structopt

---
+++ src/cli.rs
@@ -0,0 +1,16 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(name = "dhall-mock")]
+pub struct CliOpt {
+    /// Dhall configuration files to parse
+    #[structopt(required = true)]
+    pub configuration_files: Vec<String>,
+    /// http binding for server
+    #[structopt(short, long, default_value = "0.0.0.0:8088")]
+    pub http_bind: String,
+}
+
+pub fn load_cli_args() -> CliOpt {
+    CliOpt::from_args()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::env;
-
-use anyhow::{anyhow, Error};
+use anyhow::{Context, Error};
 use env_logger::Env;
-use log::info;
+use log::{debug, info, warn};
 
 use crate::compiler::{compile_configuration, load_file};
 use crate::web::State;
 use std::sync::{Arc, RwLock};
 
+mod cli;
 mod compiler;
 mod expectation;
 mod mock;
@@ -17,19 +16,31 @@ mod web;
 async fn main() -> Result<(), Error> {
     start_logger();
 
-    let args: Vec<String> = env::args().collect();
-    let filename = args
-        .get(1)
-        .ok_or(anyhow!("Program need 1 argument : File configuration path"))?;
+    let cli_args = cli::load_cli_args();
 
     info!("Start dhall mock project 👋");
-    let configuration = load_file(filename)?;
-    let expectations = compile_configuration(&configuration)?;
+
+    let expectations = cli_args
+        .configuration_files
+        .iter()
+        .flat_map(|configuration_file| {
+            debug!("Loading configuration file {}", configuration_file);
+            let configuration_result = load_file(configuration_file)
+                .and_then(|configuration| compile_configuration(&configuration))
+                .context("Error loading configuration");
+            match configuration_result {
+                Ok(expectations) => expectations.into_iter(),
+                Err(e) => {
+                    warn!("{}", e);
+                    Vec::new().into_iter()
+                }
+            }
+        })
+        .collect();
     info!("Loaded expectations : {:?}", expectations);
 
     let state = Arc::new(RwLock::new(State { expectations }));
-    // TODO load http bind from config
-    web::web_server(state.clone(), "0.0.0.0:8088".to_string()).await?;
+    web::web_server(state.clone(), cli_args.http_bind).await?;
 
     Ok(())
 }
