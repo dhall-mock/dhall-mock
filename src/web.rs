585e5b740152e50ae35079af7e14a8148c64bcee
fteychene <francois.teychene@gmail.com>
2020-04-09 10:50:08 +0200

    fixup! feat: Add web server to respond on request based on file configuration

--- src/web.rs
+++ src/web.rs
@@ -1,5 +1,5 @@
 use log::{debug, info};
-use std::convert::TryInto;
+use std::convert::TryFrom;
 use std::sync::{Arc, RwLock};
 
 use hyper::service::{make_service_fn, service_fn};
@@ -10,11 +10,11 @@ use anyhow::{anyhow, Context, Error};
 use crate::expectation::model::{Expectation, HttpMethod};
 use crate::mock::{look_for_expectation, IncomingRequest};
 
-impl TryInto<HttpMethod> for &Method {
-    type Error = Error;
+impl TryFrom<&Method> for HttpMethod {
+    type Error = anyhow::Error;
 
-    fn try_into(self) -> Result<HttpMethod, Self::Error> {
-        match self {
+    fn try_from(value: &Method) -> Result<Self, Self::Error> {
+        match value {
             &Method::GET => Ok(HttpMethod::GET),
             &Method::POST => Ok(HttpMethod::POST),
             &Method::PUT => Ok(HttpMethod::PUT),
@@ -25,20 +25,32 @@ impl TryInto<HttpMethod> for &Method {
     }
 }
 
-impl TryInto<Response<Body>> for &Expectation {
+impl<T> TryFrom<&Request<T>> for IncomingRequest {
     type Error = anyhow::Error;
 
-    fn try_into(self) -> Result<Response<Body>, Error> {
+    fn try_from(value: &Request<T>) -> Result<Self, Self::Error> {
+        Ok(IncomingRequest {
+            method: HttpMethod::try_from(value.method())?,
+            path: value.uri().path().to_string(),
+        })
+    }
+}
+
+impl TryFrom<&Expectation> for Response<Body> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &Expectation) -> Result<Response<Body>, Error> {
         Response::builder()
-            .status(self.response.status_code.unwrap_or(200))
+            .status(value.response.status_code.unwrap_or(200))
             .body(
-                self.response
+                value
+                    .response
                     .body
                     .as_ref()
                     .map(|body| Body::from(body.clone()))
                     .unwrap_or(Body::empty()),
             )
-            .context(format!("Error creating http response for {:?}", self))
+            .context(format!("Error creating http response for {:?}", value))
     }
 }
 
@@ -46,24 +58,33 @@ pub struct State {
     pub expectations: Vec<Expectation>,
 }
 
+fn not_found_response() -> Result<Response<Body>, Error> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::from("404 NotFound"))
+        .map_err(|_| anyhow!("Error creating http not found response"))
+}
+
 async fn handler<T>(req: Request<T>, state: Arc<RwLock<State>>) -> Result<Response<Body>, Error> {
-    let incoming_request = req.method().try_into().map(|method| IncomingRequest {
-        method,
-        path: req.uri().path().to_string(),
-    })?;
     let read_state = state
         .read()
         .map_err(|e| anyhow!("Error acquiring lock on state : {}", e))?;
-    look_for_expectation(&read_state.expectations, incoming_request)
-        .map(|expectation| expectation.try_into())
-        .unwrap_or(
-            Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::from("404 NotFound"))
-                .map_err(|_| anyhow!("Error creating http not found response")),
-        )
+
+    // Try converting hyper request in IncomingRequest -> Result<IncomingRequest, Error>
+    IncomingRequest::try_from(&req)
+        // Convert in a Option<IncomingRequest>
+        .ok()
+        // Search for a matching expectation
+        .and_then(|incoming_request| {
+            look_for_expectation(&read_state.expectations, incoming_request)
+        })
+        // Convert expectation in a http response (can fail) -> Option<Result<Response<Body>, Error>>
+        .map(|expectation| Response::try_from(expectation))
+        // Extract the result of the option or a not found response (that can fail too) Result<Response<Body>, Error>
+        .unwrap_or(not_found_response())
+        // Enrich error if there is one
         .context(format!(
-            "Error on creating http response for request {} {}",
+            "Error on handling http request {} {}",
             req.method(),
             req.uri().path()
         ))
